9063040759b6674e63a0bed13c3624ad6f96ebde
Dan Smith <dev@dansmith.tech>
2023-08-11 22:54:59 +0100

    Change to react-quill

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,12 @@
 "use client";
 
-import { useEffect } from "react";
+import { useState } from "react";
 
-import { useQuill } from "react-quilljs";
-import "quill/dist/quill.bubble.css";
-
-import sendFetch from "../utils/fetch";
-
-import styles from "./page.module.css";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 export default function Landing() {
-	const handleContext = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-		e.preventDefault();
+	const [value, setValue] = useState("Hello world.");
 
-		let prompt: string | undefined = window.getSelection()?.toString();
-
-		sendFetch("/api", "POST", "", { prompt }).then((res) => console.log(res));
-	};
-
-	const { Quill, quill, quillRef } = useQuill({
-		theme: "bubble",
-		modules: {
-			toolbar: "#toolbar",
-			magicUrl: true,
-		},
-		formats: ["bold", "italic", "strike", "list", "header", "clean"],
-		placeholder: "Compose an epic...",
-	});
-
-	if (Quill && !quill) {
-		const MagicUrl = require("quill-magic-url").default;
-		Quill.register("modules/magicUrl", MagicUrl);
-	}
-
-	return (
-		<main className={styles.container}>
-			<div className={styles.page}>
-				<div ref={quillRef} />
-
-				<div id="toolbar">
-					<select className="ql-header">
-						<option value="title" />
-						<option selected />
-						<option value="h2" />
-						<option value="huge" />
-					</select>
-					<button className="ql-bold" />
-					<button className="ql-italic" />
-					<button className="ql-strike" />
-					<button className="ql-list" />
-					<button className="ql-header" />
-					<button className="ql-clean" />
-					<div>frick</div>
-				</div>
-				<div id="editor" />
-			</div>
-		</main>
-	);
+	return <ReactQuill theme="snow" value={value} onChange={setValue} />;
 }

7c44dd323f6d5c5b7f3fca44c3c8ea229b7628a3
Dan Smith <dev@dansmith.tech>
2023-08-16 09:51:10 +0000

    Update dependencies

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,16 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import { client } from "@gradio/client";
-
 export async function POST(request: NextRequest) {
 	const { prompt, input } = await request.json();
 
-	const app = await client(
-		"https://ysharma-explore-llamav2-with-tgi.hf.space/"
-	);
-	const result = (await app.predict("/chat", [
-		"Howdy!", // string  in 'Message' Textbox component
-	])) as string;
+	const result = {};
 
 	return NextResponse.json(result);
 }

121cc64addc24eb5eaa0461397b41b8fbf665f4b
Dan <dev@dansmith.tech>
2023-09-22 12:33:44 +0100

    Implement OpenAI API

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,32 +1,46 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const { Configuration, OpenAIApi } = require("openai");
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-	apiKey: "YOUR_API_KEY",
+	apiKey: process.env.OPENAI_API_KEY,
 });
+const openai = new OpenAIApi(configuration);
 
-async function getAiResponse(topic: string) {
-	const openai = new OpenAIApi(configuration);
-	const completion = await openai.createCompletion({
-		model: "text-davinci-003",
-		prompt: topic,
-		max_tokens: 1024,
-		n: 1,
-		stop: null,
-		temperature: 0.7,
+async function getAiResponse(prompt: string) {
+	const response = await fetch("https://api.openai.com/v1/chat/completions", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+		},
+		body: JSON.stringify({
+			model: "gpt-3.5-turbo",
+			messages: [{ role: "user", content: prompt }],
+			temperature: 0.7,
+			top_p: 1,
+			frequency_penalty: 0,
+			presence_penalty: 0,
+			max_tokens: 200,
+			stream: false,
+			n: 1,
+		}),
 	});
-	console.log(completion.data.choices[0].text);
+
+	const json = await response.json();
+	return json.choices[0].message.content;
 }
 
 export async function POST(request: NextRequest) {
 	const { prompt, input } = await request.json();
 	const toSend = prompt + "\n'" + input + "'";
+	const answer = await getAiResponse(toSend);
+
+	// console.log(answer);
 
 	return NextResponse.json({
 		prompt,
 		input,
-		answer:
-			"Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer congue condimentum augue, ut laoreet nunc vulputate eu. Aenean scelerisque tortor nec ligula tincidunt auctor. Donec in mi sed tellus fringilla ultrices at in elit. Suspendisse potenti. Vivamus accumsan venenatis orci, id venenatis ante suscipit a. Suspendisse at metus non arcu sagittis facilisis. Quisque hendrerit ac libero non bibendum.",
+		answer,
 	});
 }

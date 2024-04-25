c7ecf67b8c083cde4cb6f3746e1f4e65b8168f9e
Dan <dev@dansmith.tech>
2024-04-25 23:01:33 +0100

    Change links to horizontal fixed container

--- components/wrappers/Navigator.tsx
+++ components/wrappers/Navigator.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link";
 
-import { BiSolidCoinStack, BiSolidFileBlank, BiUser } from "react-icons/bi";
+import {
+	BiSolidChat,
+	BiSolidCog,
+	BiSolidCoinStack,
+	BiSolidFileBlank,
+	BiUser,
+} from "react-icons/bi";
 
 import styles from "./Navigator.module.css";
 
@@ -8,41 +14,65 @@ const Navigator = ({
 	selected,
 	children,
 }: {
-	selected: "files" | "credits";
+	selected: "files" | "prompts" | "credits" | "settings";
 	children: React.ReactNode;
 }) => {
 	return (
 		<div className={styles.containerMain}>
 			<div className={styles.containerContent}>
-				<div className={styles.containerLinks}>
+				<div className={styles.content}>{children}</div>
+				<div className={styles.containerNav}>
 					<div className={styles.account}>
 						<BiUser />
 						User
 					</div>
-					<Link
-						href="/home"
-						className={
-							styles.link +
-							" " +
-							(selected === "files" ? styles.selected : "")
-						}
-					>
-						<BiSolidFileBlank />
-						Files
-					</Link>
-					<Link
-						href="/credits"
-						className={
-							styles.link +
-							" " +
-							(selected === "credits" ? styles.selected : "")
-						}
-					>
-						<BiSolidCoinStack />
-						Credits
-					</Link>
+					<div className={styles.containerLinks}>
+						<Link
+							href="/home"
+							className={
+								styles.link +
+								" " +
+								(selected === "files" ? styles.selected : "")
+							}
+						>
+							<BiSolidFileBlank />
+							Files
+						</Link>
+						<Link
+							href="/prompts"
+							className={
+								styles.link +
+								" " +
+								(selected === "prompts" ? styles.selected : "")
+							}
+						>
+							<BiSolidChat />
+							Prompts
+						</Link>
+						<Link
+							href="/credits"
+							className={
+								styles.link +
+								" " +
+								(selected === "credits" ? styles.selected : "")
+							}
+						>
+							<BiSolidCoinStack />
+							Credits
+						</Link>
+						<Link
+							href="/settings"
+							className={
+								styles.link +
+								" " +
+								(selected === "settings" ? styles.selected : "")
+							}
+						>
+							<BiSolidCog />
+							Settings
+						</Link>
+					</div>
 				</div>
-				<div className={styles.content}>{children}</div>
 			</div>
 		</div>
 	);
